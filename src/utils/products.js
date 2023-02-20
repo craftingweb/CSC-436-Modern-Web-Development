f7f763a4b0f7b281b60a1bbfe35cc1fdd3d2e348
IT <it@ITs-MacBook-Air.local>
2023-02-20 18:51:17 -0500

    homework 3

---
+++ src/utils/products.js
@@ -0,0 +1,9 @@
+const products = [
+  { id: 1, name: "Apple iPhone", price: 999, onSale: false },
+  { id: 2, name: "Samsung Galaxy S21", price: 799, onSale: true },
+  { id: 3, name: "Google Pixel 5", price: 699, onSale: false },
+  { id: 4, name: "OnePlus 9 Pro", price: 899, onSale: true },
+  { id: 5, name: "AirPods Pro", price: 249, onSale: false },
+];
+
+export default products;
